acb6ad725f787fe460047f2f3e05f98e270fa936
zenoone <zenoone@physik.fu-berlin.de>
2017-11-25 14:29:47 +0100

    Indentations cleared up
    Hope I got this right this time.

--- urbanbroccoli/sor.py
+++ urbanbroccoli/sor.py
@@ -31,27 +31,22 @@ def sor_1d(rho, hx, epsilon, maxiter, maxerr, w):
     return phi
 
 def sor_2d(rho, hx, hy, epsilon, maxiter, maxerr, w):
-    """Performs a sucsessive over-relaxation for the Poisson equation of a two dimensional discretized charge distribution:
-          Parameters:
-            rho (ndarray)
-                insert a two dimensional numpy array representing a discretized charge distribution
-            hx (float)
-                grid spacing in x direction
-            hy (float)
-                grid spacing in y direction
-            epsilon (float)
-                permeativity constant
-            maxiter (integer)
-                maximum number of iterations
-            maxerr (float)
-                minimal update-stepsize
-            w (float)
-                mixing constant determinig the ammount of new and old values used for the updated iterartion step
-          
-          Returns:
-            phi (ndarray) 
-                returns the resulting potential phi as a two dimensional numpy array
-          
+    """Performs a 2D sucsessive over-realxation.
+    
+    Performs a sucsessive over-relaxation for the Poisson equation to determine the 
+    potential resulting from a two dimensional discretized charge distribution.
+        
+        Parameters:
+            rho (ndarray): two dimensional discretized charge distribution
+            hx (float): grid spacing in x direction
+            hy (float): grid spacing in y direction
+            epsilon (float): permeativity constant
+            maxiter (integer): maximum number of iterations
+            maxerr (float): minimal update-stepsize
+            w (float): mixing constant of new and old values in the iterartion step
+
+        Returns:
+            phi (ndarray): resulting potential phi as a two dimensional numpy array
           """
     if rho.ndim != 2:
         raise ValueError("rho must be of shape=(nx, ny)")
