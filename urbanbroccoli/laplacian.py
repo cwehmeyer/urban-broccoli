08eeb68b6a4673afcc552bfb80bb1041e9714abb
Maximilian Salomon <32635839+BioFreak95@users.noreply.github.com>
2017-11-28 10:13:55 +0100

    Update laplacian.py
    add documentation

--- urbanbroccoli/laplacian.py
+++ urbanbroccoli/laplacian.py
@@ -1,5 +1,12 @@
 from numpy import zeros, float64
 
+''' 
+Generates an laplace sparse matrix of an 1D-vector.
+
+Arguments:
+nx (integer): length of the vector
+hx (integer): length of the grid in x:x
+'''
 def create_laplacian_1d(nx, hx):
     laplacian = zeros(shape=(nx, nx), dtype=float64)
     mx = 1.0 / (hx * hx)
@@ -39,6 +46,13 @@ def create_laplacian_3d(nx, ny, nz, hx, hy, hz):
                 laplacian[x, y, z, x, y, (z - 1) % nz] += mz
     return laplacian.reshape(nx * ny * nz, nx * ny * nz)
 
+''' 
+Generates an laplace sparse matrix of an 1D, 2D or 3D input. Delegates the task to the respective function.
+
+Arguments:
+n (array): vector with 1-3 Dimensions
+h (array): length of the grid in 1-3 Dimensions
+'''
 def create_laplacian(n, h):
     if len(n) == 1:
         return create_laplacian_1d(*n, *h)
